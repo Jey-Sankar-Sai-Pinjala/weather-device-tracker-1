569214103a30836ee2b5edd8236ad8f25672c935
jey <165001244+Jey-Sankar-Sai-Pinjala@users.noreply.github.com>
2025-05-22 12:50:51 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -25,5 +25,6 @@ def parse_csv(filepath):
 def get_positions():
     return jsonify(parse_csv('positions.csv'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
